37ffbbc9024b16a2053ba7c1f3e8c48d3c591ae9
Jason Buckley <jbuckley@csumb.edu>
2021-03-30 18:47:53 -0700

    added more info columns to Mother and Father tables

--- db.sql
+++ db.sql
@@ -1,62 +1,70 @@
 CREATE TABLE User(
     user_id INT NOT NULL AUTO_INCREMENT,
-    user_email VARCHAR(384),
-    user_password VARCHAR(128),
+    user_email VARBINARY(412),
+    user_password VARBINARY(156),
 
     PRIMARY KEY(user_id)
 );
 
 CREATE TABLE Adminstrator(
     admin_id INT NOT NULL AUTO_INCREMENT,
-    admin_email VARCHAR(384),
+    admin_email VARBINARY(412),
 
-    PRIMARY KEY(admin_id);
-)
+    PRIMARY KEY(admin_id)
+);
 
 CREATE TABLE Father(
     father_id INT NOT NULL AUTO_INCREMENT,
-    father_first_name VARCHAR(64),
-    father_last_name VARCHAR(64),
-    father_phone_number VARCHAR(10),
-    father_email VARCHAR(384),
+    father_first_name VARBINARY(92),
+    father_last_name VARBINARY(92),
+    father_phone_number VARBINARY(38),
+    father_email VARBINARY(412),
+    father_education VARBINARY(156),
+    father_place_birth VARBINARY(283),
+    father_employer VARBINARY(283),
+    father_job_title VARBINARY(283),
 
     PRIMARY KEY(father_id)
 );
 
 CREATE TABLE Mother(
     mother_id INT NOT NULL AUTO_INCREMENT,
-    mother_first_name VARCHAR(64),
-    mother_last_name VARCHAR(64),
-    mother_phone_number VARCHAR(10),
-    mother_email VARCHAR(384),
+    mother_first_name VARBINARY(92),
+    mother_last_name VARBINARY(92),
+    mother_phone_number VARBINARY(38),
+    mother_email VARBINARY(412),
+    mother_education VARBINARY(156),
+    mother_place_birth VARBINARY(283),
+    mother_employer VARBINARY(283),
+    mother_job_title VARBINARY(283),
 
     PRIMARY KEY(mother_id)
 );
 
 CREATE TABLE Guardian(
     guardian_id INT NOT NULL AUTO_INCREMENT,
-    guardian_name VARCHAR(128),
-    guardian_email VARCHAR(384),
-    guardian_phone_number VARCHAR(10),
+    guardian_name VARBINARY(156),
+    guardian_email VARBINARY(412),
+    guardian_phone_number VARBINARY(38),
 
     PRIMARY KEY(guardian_id)
 );
 
 CREATE TABLE Student(
     student_id INT NOT NULL AUTO_INCREMENT,
-    student_email VARCHAR(384),
-    student_first_name VARCHAR(64),
-    student_middle_name VARCHAR(64),
-    student_last_name VARCHAR(64),
-    student_phone_number VARCHAR(10),
+    student_email VARBINARY(412),
+    student_first_name VARBINARY(92),
+    student_middle_name VARBINARY(92),
+    student_last_name VARBINARY(92),
+    student_phone_number VARBINARY(38),
     father_id INT,
     mother_id INT,
     guardian_id INT,
-    student_grade INT,
-    student_status VARCHAR(64),
+    student_grade VARBINARY(30),
+    student_status VARBINARY(92),
 
     PRIMARY KEY(student_id),
-    FOREIGN KEY(father_id) REFERENCES Father (father_id) ON DELETE CASCADE,
-    FOREIGN KEY(mother_id) REFERENCES Mother (mother_id) ON DELETE CASCADE,
-    FOREIGN KEY(guardian_id) REFERENCES Guardian (guardian_id) ON DELETE CASCADE
+    CONSTRAINT father_id_fk FOREIGN KEY(father_id) REFERENCES Father (father_id),
+    CONSTRAINT mother_id_fk FOREIGN KEY(mother_id) REFERENCES Mother (mother_id),
+    CONSTRAINT guardian_id_fk FOREIGN KEY(guardian_id) REFERENCES Guardian (guardian_id)
 );
